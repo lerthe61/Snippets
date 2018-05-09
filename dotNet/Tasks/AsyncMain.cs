9759ad83911da143912adbacec1c3936facaf520
Dmytro Panchenko <lerthe61@ukr.net>
2018-05-09 17:24:57 -0400

    Add async Main snippet

--- dotNet/Tasks/AsyncMain.cs
+++ dotNet/Tasks/AsyncMain.cs
@@ -8,3 +8,31 @@ class Program
         }).GetAwaiter().GetResult();
     }
 }
+
+// OR
+
+private static int Main(string[] args)
+{
+    var cts = new CancellationTokenSource();
+    Console.CancelKeyPress += (s, e) =>
+    {
+        e.Cancel = !cts.IsCancellationRequested;
+        cts.Cancel();
+    };
+
+    try
+    {
+        return MainAsync(args, cts.Token).GetAwaiter().GetResult();
+    }
+    catch (OperationCanceledException)
+    {
+        return 1223; // Cancelled.
+    }
+}
+
+private static async Task<int> MainAsync(string[] args, CancellationToken cancellationToken)
+{
+    // Your code...
+
+    return await Task.FromResult(0); // Success.
+}
