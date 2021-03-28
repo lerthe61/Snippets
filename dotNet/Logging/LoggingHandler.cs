62f9ccfb5ad132f629b9d75563159d6285013fe0
Dmytro Panchenko <dpanchenko@osisoft.com>
2021-03-27 22:48:16 -0400

    Http(s) logging handler to use with conjunction with HttpClient

---
+++ dotNet/Logging/LoggingHandler.cs
@@ -0,0 +1,32 @@
+    // Handler that will log all requests/responses
+    // Usage: HttpClient client = new HttpClient(new LoggingHandler());
+    public class LoggingHandler : DelegatingHandler
+    {        
+        public LoggingHandler(HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+        }
+     
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            System.Diagnostics.Debug.WriteLine("Request:");
+            System.Diagnostics.Debug.WriteLine(request.ToString());
+            if (request.Content != null)
+            {
+                System.Diagnostics.Debug.WriteLine(await request.Content.ReadAsStringAsync());
+            }
+            System.Diagnostics.Debug.WriteLine("");
+
+            HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
+
+            System.Diagnostics.Debug.WriteLine("Response:");
+            System.Diagnostics.Debug.WriteLine(response.ToString());
+            if (response.Content != null)
+            {
+                System.Diagnostics.Debug.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+            System.Diagnostics.Debug.WriteLine("");
+
+            return response;
+        }
+    }
